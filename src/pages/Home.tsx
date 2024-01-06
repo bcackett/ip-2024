7e20e986afd0454de2b7bfe24f53666acc59590e
btc21 <btc21@ic.ac.uk>
2024-01-06 18:11:22 +0000

    Add buttons to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,28 @@
+import { useNavigate } from "react-router-dom";
+
 function Welcome() {
+  const nav = useNavigate();
+
+  const goToPlay = () => {
+    nav("/play")
+  }
+
+  const goToTrain = () => {
+    nav("/train")
+  }
   return (
     <>
       <div className="name-frame">
         <h1>Welcome to I.Poker!</h1>
+      </div>
+      <div>
+        <label>I.Poker is a one-site-fits-all solution to learning and improving at Texas Hold 'Em Poker!</label>
+        <button className="spaced-button" type="button" onClick={goToTrain}>
+          "Training Lessons"
+        </button>
+        <button className="spaced-button" type="button" onClick={goToPlay}>
+          "Play Online"
+        </button>
       </div>
     </>
   );
