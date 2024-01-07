96f1f88f71b7f6d11e7bf1ccbe08b5ee271309b6
btc21 <btc21@ic.ac.uk>
2024-01-07 15:22:20 +0000

    Reconstruct home.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,44 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 import NotFound from './pages/NotFound';
 import { useNavigate } from "react-router-dom";
 
 function App() {
-  const nav = useNavigate();
-
-  const goToPlay = () => {
-    nav("/play")
-  }
-
-  const goToTrain = () => {
-    nav("/train")
-  }
-
   return (
-    <>
-      <div className="name-frame">
-        <h1>Welcome to I.Poker!</h1>
-      </div>
-      <div>
-        <label>I.Poker is a one-site-fits-all solution to learning and improving at Texas Hold 'Em Poker!</label>
-      </div>
-      <div>
-        <button className="spaced-button" type="button" onClick={goToTrain}>
-          "Training Lessons"
-        </button>
-        <button className="spaced-button" type="button" onClick={goToPlay}>
-          "Play Online"
-        </button>
-      </div>
-    </>
-    // <Routes>
-    //   {/* <Route path="/train" element={<Training />} />
-    //   <Route path="/play" element={<Play />} /> */}
-    //   <Route path="/" element={<Home />} />
-    //   <Route path="/*" element={<NotFound />} />
-    // </Routes>
+    <Routes>
+      {/* <Route path="/train" element={<Training />} />
+      <Route path="/play" element={<Play />} /> */}
+      <Route path="/" element={<Home />} />
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   );
 }
 
