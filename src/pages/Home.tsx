52e8cfcb446019b5b2dd2fff6916641f78b81f7f
btc21 <btc21@ic.ac.uk>
2024-01-07 15:16:08 +0000

    Test commit for code in home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,33 @@
-import { useNavigate } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
 
-function Welcome() {
-  const nav = useNavigate();
+// function Welcome() {
+//   const nav = useNavigate();
 
-  const goToPlay = () => {
-    nav("/play")
-  }
+//   const goToPlay = () => {
+//     nav("/play")
+//   }
 
-  const goToTrain = () => {
-    nav("/train")
-  }
-  return (
-    <>
-      <div className="name-frame">
-        <h1>Welcome to I.Poker!</h1>
-      </div>
-      {/* <div>
-        <label>I.Poker is a one-site-fits-all solution to learning and improving at Texas Hold 'Em Poker!</label>
-      </div>
-      <div>
-        <button className="spaced-button" type="button" onClick={goToTrain}>
-          "Training Lessons"
-        </button>
-        <button className="spaced-button" type="button" onClick={goToPlay}>
-          "Play Online"
-        </button>
-      </div> */}
-    </>
-  );
-}
+//   const goToTrain = () => {
+//     nav("/train")
+//   }
+//   return (
+//     <>
+//       <div className="name-frame">
+//         <h1>Welcome to I.Poker!</h1>
+//       </div>
+//       <div>
+//         <label>I.Poker is a one-site-fits-all solution to learning and improving at Texas Hold 'Em Poker!</label>
+//       </div>
+//       <div>
+//         <button className="spaced-button" type="button" onClick={goToTrain}>
+//           "Training Lessons"
+//         </button>
+//         <button className="spaced-button" type="button" onClick={goToPlay}>
+//           "Play Online"
+//         </button>
+//       </div>
+//     </>
+//   );
+// }
 
-export default Welcome;
+export {}
