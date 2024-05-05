9729782e1db184a026379c269a1aa49d0b79cb32
Ben Cackett <91209318+bcackett@users.noreply.github.com>
2024-05-05 12:07:18 +0100

    Fix bug where cancelling number of players input for local multiplayer does nothing

--- src/pages/Play.tsx
+++ src/pages/Play.tsx
@@ -1,12 +1,21 @@
+import { useNavigate } from "react-router-dom";
 import Board from "../elements/Board";
 
 function Play() {
-  let amount = Number(window.prompt("How many players are participating? Minimum 2, maximum 8."));
-  while (amount < 2 || amount > 8) {
-    amount = Number(window.prompt("How many players are participating? Minimum 2, maximum 8."))
+  let nullableAmount = window.prompt("How many players are participating? Minimum 2, maximum 8.");
+  let amount = Number(nullableAmount);
+  while ((amount < 2 || amount > 8) && nullableAmount !== null) {
+    nullableAmount = window.prompt("How many players are participating? Minimum 2, maximum 8.");
+    amount = Number(nullableAmount);
   }
-  return (
-    <Board totalPlayers={amount} computerPlayers={0}/>
-  );
+
+  if (nullableAmount === null) {
+    window.location.assign(window.location.href.slice(0, -4));
+    return (null);
+  } else {
+    return (
+      <Board totalPlayers={amount} computerPlayers={0}/>
+    );
+  }
 }
 export default Play;
