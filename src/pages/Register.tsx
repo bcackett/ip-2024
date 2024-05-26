df0f9653394ecfde6e97383bfc0923c1514653b7
Ben Cackett <ben.cackett@sky.com>
2024-05-26 20:27:18 +0100

    Fix bug where unnecessary decryption occurs when registering

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -53,7 +53,7 @@ function Register() {
               if (firstName.length > 0) {
                 const e4 = await supabase.from("logins").update({firstName: cipher.encode(firstName, "name")}).eq("userID", newUserID);
                 if (e4.error) {throw e4.error};
-                sessionStorage.setItem("name", cipher.decode(firstName, "name")); 
+                sessionStorage.setItem("name", firstName); 
               }
               goToHome();
             }
